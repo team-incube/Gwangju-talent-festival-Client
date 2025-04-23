407f190438dd40205a90a54878472dbba30f31f3
송재욱 <haensol976520@gmail.com>
2025-04-23 21:01:12 +0900

    feat: clsx 적용

--- src/views/signup/ui/SignupPage/index.tsx
+++ src/views/signup/ui/SignupPage/index.tsx
@@ -1,16 +1,13 @@
 import { Logo } from "@/shared/asset/svg/Logo";
 import { colors } from "@/shared/utils/color";
 import SignupFormContainer from "@/widgets/signup/ui/SignupFormContainer";
-
+import { cn } from "@/shared/utils/cn";
 
 const SignupPage = () => {
   return (
-    <div
-      className="flex flex-col items-center mt-52
-     h-screen w-full px-4"
-    >
-      <div className="w-full max-w-md flex flex-col items-center">
-        <div className="mb-20">
+    <div className={cn("flex flex-col items-center mt-52 h-screen w-full px-4")}>
+      <div className={cn("w-full max-w-md flex flex-col items-center")}>
+        <div className={cn("mb-20")}>
           <Logo color={colors.main[600]} size={200} />
         </div>
         <SignupFormContainer />
--- src/widgets/signup/ui/SignupFormContainer/index.tsx
+++ src/widgets/signup/ui/SignupFormContainer/index.tsx
@@ -1,21 +1,26 @@
 import Input from "@/shared/ui/Input";
 import Button from "@/shared/ui/Button";
+import { cn } from "@/shared/utils/cn";
 
 const SignupFormContainer = () => {
   return (
-    <div className="w-full space-y-16 mb-4">
-      <div className="flex flex-col gap-2">
-        <label className="text-sm font-medium">전화번호</label>
-        <div className="flex items-end gap-8">
-          <div className="w-full">
+    <div className={cn("w-full space-y-16 mb-4")}>
+      <div className={cn("flex flex-col gap-2")}>
+        <label className={cn("text-sm font-medium")}>전화번호</label>
+        <div className={cn("flex items-end gap-8")}>
+          <div className={cn("w-full")}>
             <Input type="text" placeholder="전화번호를 입력해주세요." />
           </div>
-          <Button className="w-32 shrink-0 h-[50px]">인증번호</Button>
+          <Button className={cn("w-32 shrink-0 h-[50px]")}>인증번호</Button>
         </div>
       </div>
 
-      <Input type="text" placeholder="인증번호를 입력해주세요." label="인증번호 입력" className="mt-2" />
-      <Input type="password" placeholder="비밀번호를 입력해주세요." label="비밀번호 입력" className="mt-2" />
+      <Input type="text" placeholder="인증번호를 입력해주세요." label="인증번호 입력" className={cn("mt-2")} />
+      <Input type="password" placeholder="비밀번호를 입력해주세요." label="비밀번호 입력" className={cn("mt-2")} />
+
+      <div className={cn("flex flex-col gap-2")}>
+        <Button className={cn("w-full mt-32")}>회원가입</Button>
+      </div>
     </div>
   );
 };
