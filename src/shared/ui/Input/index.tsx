90cd00c78f3045dd074021e5d1fe8dd1c5086077
bae080311 <s24024@gsm.hs.kr>
2025-04-28 16:24:21 +0900

    Fix: input component label font

--- src/shared/ui/Input/index.tsx
+++ src/shared/ui/Input/index.tsx
@@ -5,25 +5,27 @@ type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
   label?: string;
 };
 
-const Input = forwardRef<HTMLInputElement, InputProps>(({ className, type, placeholder, label, ...props }, ref) => {
-  return (
-    <div className="w-full">
-      <label className="text-sm font-medium">{label}</label>
-      <input
-        className={cn(
-          "w-full border-none outline-none rounded-md px-16 py-4 text-body3r placeholder:text-gray-400 focus:ring-0 transition-all h-[50px] bg-gray-100",
-          type === "number" &&
-            "[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none",
-          className
-        )}
-        placeholder={placeholder}
-        type={type}
-        ref={ref}
-        {...props}
-      />
-    </div>
-  );
-});
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, placeholder, label, ...props }, ref) => {
+    return (
+      <div className="w-full">
+        <label className="text-body3b">{label}</label>
+        <input
+          className={cn(
+            "w-full border-none outline-none rounded-md px-16 py-4 text-body3r placeholder:text-gray-400 focus:ring-0 transition-all h-[50px] bg-gray-100",
+            type === "number" &&
+              "[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none",
+            className
+          )}
+          placeholder={placeholder}
+          type={type}
+          ref={ref}
+          {...props}
+        />
+      </div>
+    );
+  }
+);
 
 Input.displayName = "Input";
 
