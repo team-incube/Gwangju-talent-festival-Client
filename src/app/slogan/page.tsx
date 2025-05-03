880bd413ce88aefac365ff347bd1980751f58642
bae080311 <s24024@gsm.hs.kr>
2025-05-03 16:06:10 +0900

    fix: margin

--- src/app/slogan/page.tsx
+++ src/app/slogan/page.tsx
@@ -3,7 +3,7 @@ import SloganPage from "@/views/slogan/ui/SloganPage";
 
 export default function Slogan() {
   return (
-    <div className={cn("flex justify-center mt-[2.75rem] mb-[3.5rem]")}>
+    <div className={cn("flex justify-center")}>
       <SloganPage />
     </div>
   );
