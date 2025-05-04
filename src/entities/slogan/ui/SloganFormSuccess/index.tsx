0d5431a5d5684614ecb673c31603c7b8bf7c523f
bae080311 <s24024@gsm.hs.kr>
2025-05-05 03:13:31 +0900

    refactor: 성공화면 분리

---
+++ src/entities/slogan/ui/SloganFormSuccess/index.tsx
@@ -0,0 +1,23 @@
+import Share from "@/shared/asset/Share";
+import { Logo } from "@/shared/asset/svg/Logo";
+import { colors } from "@/shared/utils/color";
+
+export default function SloganFormSuccess() {
+  return (
+    <div
+      className="flex flex-col items-center justify-center w-full"
+      style={{ height: "calc(100vh - 70px)" }}
+    >
+      <Logo height={131} color={colors.main[600]} width={211} />
+      <div className="mt-[52px]">
+        <h1 className="text-title1b  text-main-600">응모가 완료되었습니다!</h1>
+        <div className="flex gap-24 items-center justify-center mt-32">
+          <Share width={37} height={36} />
+          <span className="text-body1r underline">
+            친구들에게도 공유해주세요
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
