f9e7bcb4cc6c22bd7cf1447b6b1f5f7ca1ee6ffe
bae080311 <s24024@gsm.hs.kr>
2025-05-10 16:41:41 +0900

    style: text style 변경

--- src/entities/slogan/ui/SloganFormSuccess/index.tsx
+++ src/entities/slogan/ui/SloganFormSuccess/index.tsx
@@ -10,10 +10,12 @@ export default function SloganFormSuccess() {
     >
       <Logo height={131} color={colors.main[600]} width={211} />
       <div className="mt-[52px]">
-        <h1 className="sm:text-title1b text-title4b  text-main-600">응모가 완료되었습니다!</h1>
+        <h1 className="sm:text-title2b text-title4b">응모가 완료되었습니다!</h1>
         <div className="flex gap-24 items-center justify-center mt-12 sm:mt-24">
-          <Share className="w-25 h-24 sm:w-[37px] sm:h-[36px]" />
-          <span className="sm:text-body1r text-body3r underline">친구들에게도 공유해주세요</span>
+          <Share color={colors.main[600]} height={24} width={24} />
+          <span className="text-body2r text- underline text-main-600">
+            친구들에게도 공유해주세요
+          </span>
         </div>
       </div>
     </div>
