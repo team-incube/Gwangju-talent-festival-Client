f7a88f31b83b96ea2a08893e37cc5daff0655cc5
bae080311 <s24024@gsm.hs.kr>
2025-05-10 17:26:50 +0900

    fix: name

--- src/entities/slogan/lib/share.ts
+++ src/entities/slogan/lib/share.ts
@@ -3,7 +3,7 @@ import { toast } from "sonner";
 export const share = async () => {
   try {
     await navigator.share({
-      title: "광주탈렌트페스티벌 슬로건 공모전 참여하기",
+      title: "광주학생탈렌트페스티벌 슬로건 공모전 참여하기",
       url: "https://www.xn--hc0b809dz3b.kr/slogan",
     });
     toast.success("공유하기에 성공했습니다.");
