bb3ef7617cfb44d8e9440d95cbf131b4797bb431
bae080311 <s24024@gsm.hs.kr>
2025-05-10 16:47:45 +0900

    feat: 공유하기 지원하지 않는 브라우저이면 hidden

--- src/entities/slogan/ui/SloganFormSuccess/index.tsx
+++ src/entities/slogan/ui/SloganFormSuccess/index.tsx
@@ -12,12 +12,14 @@ export default function SloganFormSuccess() {
       <Logo height={131} color={colors.main[600]} width={211} />
       <div className="mt-[52px]">
         <h1 className="sm:text-title2b text-title4b">응모가 완료되었습니다!</h1>
-        <div onClick={share} className="flex gap-24 items-center justify-center mt-12 sm:mt-24">
-          <Share color={colors.main[600]} height={24} width={24} />
-          <span className="text-body2r text- underline text-main-600">
-            친구들에게도 공유해주세요
-          </span>
-        </div>
+        {typeof navigator.share !== "undefined" && (
+          <div onClick={share} className="flex gap-24 items-center justify-center mt-12 sm:mt-24">
+            <Share color={colors.main[600]} height={24} width={24} />
+            <span className="text-body2r text- underline text-main-600">
+              친구들에게도 공유해주세요
+            </span>
+          </div>
+        )}
       </div>
     </div>
   );
