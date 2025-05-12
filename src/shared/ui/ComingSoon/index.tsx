ea08bd90a39fd2792bc5e5ea58f986ef25f37619
happytaeyoon <s23021@gsm.hs.kr>
2025-05-12 12:21:40 +0900

    fix: 멘트 수정

--- src/shared/ui/ComingSoon/index.tsx
+++ src/shared/ui/ComingSoon/index.tsx
@@ -8,7 +8,7 @@ export default function ComingSoon() {
       style={{ height: "calc(100vh - 81px)" }}
     >
       <Tool color={colors.main[600]} />
-      <h1 className="text-title4b mt-[44px] mb-24 text-main-600">현재 개발 중입니다</h1>
+      <h1 className="text-title4b mt-[44px] mb-24 text-main-600">현재 신청기간 전 입니다</h1>
       <p className="text-body3r">5월 19일 소개 페이지 오픈, 5월 26일 슬로건 공모 오픈</p>
     </div>
   );
