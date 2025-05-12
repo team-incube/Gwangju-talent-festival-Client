de6c065cc80f0f577dd4d46615285c18b1c1f01c
happytaeyoon <s23021@gsm.hs.kr>
2025-05-12 12:29:47 +0900

    add: 멘트 추가

--- src/shared/ui/ComingSoon/index.tsx
+++ src/shared/ui/ComingSoon/index.tsx
@@ -8,8 +8,13 @@ export default function ComingSoon() {
       style={{ height: "calc(100vh - 81px)" }}
     >
       <Tool color={colors.main[600]} />
-      <h1 className="text-title4b mt-[44px] mb-24 text-main-600">현재 신청기간 전 입니다</h1>
-      <p className="text-body3r">5월 19일 소개 페이지 오픈, 5월 26일 슬로건 공모 오픈</p>
+      <h1 className="text-title4b mt-[44px] mb-24 text-main-600">현재 신청기간 전입니다.</h1>
+      <p className="text-body2b mobile:text-body3b">
+        문의사항은 우측 하단 문의하기를 이용해 주세요.
+      </p>
+      <p className="text-body3r mobile:text-caption1r mb-[100px]">
+        5월 19일 소개 페이지 오픈, 5월 26일 슬로건 공모 오픈
+      </p>
     </div>
   );
 }
