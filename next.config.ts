1453b1f05e1200a4674b4e044462154bef9c49cd
태윤 <129380797+happytaeyoon@users.noreply.github.com>
2025-05-16 10:59:29 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ["www.광탈페.kr", "www.xn--hc0b809dz3b.kr"],
-  },
 };
 
 export default nextConfig;
