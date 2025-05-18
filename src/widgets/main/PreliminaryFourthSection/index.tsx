4be9bb45fdd2966c300842eae8dce08a6358eb56
재욱 <haensol976520@gmail.com>
2025-05-18 15:04:07 +0900

    fix: 예선 간격 조정

--- src/widgets/main/PreliminaryFourthSection/index.tsx
+++ src/widgets/main/PreliminaryFourthSection/index.tsx
@@ -30,34 +30,34 @@ const PreliminaryFourthSection = () => {
           )}
         >
           <div className={cn("w-[25%] mobile:w-full mobile:px-16")}>
-            <p className={cn("text-title4b mobile:text-body2b place-self-start mb-24")}>
+            <p className={cn("text-title4b mobile:text-body3b place-self-start mb-24 mobile:mb-0")}>
               1차 영상 심사
             </p>
-            <p className={cn("text-body2r text-gray-500 mobile:text-body3r mobile:py-[1rem]")}>
+            <p className={cn("text-body2r text-gray-500 mobile:text-caption2r mobile:py-8")}>
               신청서 내용과 제출영상을 바탕으로 전문위원 심사를 거쳐 예선전(光트로) 심사출연자를
               확정합니다.
             </p>
           </div>
-          <div className={cn("w-[70%] mobile:w-full")}>
+          <div className={cn("w-[70%] mobile:w-full mobile:mt-16")}>
             <ImageCarousel wide={true} slides={SLIDES_1} />
           </div>
         </div>
         <div className={cn("flex w-full items-start justify-between mobile:flex-col-reverse")}>
-          <div className={cn("w-[70%] mobile:w-full")}>
+          <div className={cn("w-[70%] mobile:w-full mobile:mt-16")}>
             <ImageCarousel wide={true} slides={SLIDES_2} />
           </div>
           <div className={cn("w-[25%] mobile:w-full mobile:px-16")}>
-            <p className={cn("text-title4b mobile:text-body2b place-self-start mb-24")}>
+            <p className={cn("text-title4b mobile:text-body3b place-self-start mb-24 mobile:mb-0")}>
               2차 예선(光트로)
             </p>
             <button
               className={cn(
-                "inline-flex items-center text-main-600 font-bold text-body2b mobile:text-sm hover:underline group",
+                "inline-flex items-center text-main-600 font-bold text-body2b mb-24 mobile:mb-0 mobile:text-sm hover:underline group",
               )}
             >
               2025. 7. 25(금)~26.(토) 광주광역시교육청학생교육문화회관{" "}
             </button>
-            <p className={cn("text-body2r text-gray-500 mobile:text-body3r mobile:py-[1rem]")}>
+            <p className={cn("text-body2r text-gray-500 mobile:text-caption2r mobile:py-8")}>
               1차 영상심사를 통과한 참가자들이 직접 무대에서 경연을 펼치게 됩니다. 전문 심사위원의
               심사를 거쳐 본선 진출자를 확정하게 됩니다.
             </p>
