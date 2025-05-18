6a74f06b521b2c01fd2f85012cfc496adddf5fa9
happytaeyoon <s23021@gsm.hs.kr>
2025-05-19 08:00:37 +0900

    fix 이미지 경로 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
       "光탈페(광주학생탈렌트페스티벌)은 광주학생의회가 중심이 되어 학생들이 재능을 펼치고 즐길 수 있도록 프로그램을 기획하고 진행하는 학생주도형 오디션 프로그램입니다.",
     url: "https://www.광탈페.kr",
     siteName: "광주학생탈렌트페스티벌",
-    images: ["https://www.광탈페.kr/og.png"],
+    images: ["https://www.광탈페.kr/images/og.png"],
   },
 };
 
