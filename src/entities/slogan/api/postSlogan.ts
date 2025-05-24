19db47678392208afaa971f6dc14d2aea280d52e
bae080311 <s24024@gsm.hs.kr>
2025-05-24 21:33:52 +0900

    fix: type

--- src/entities/slogan/api/postSlogan.ts
+++ src/entities/slogan/api/postSlogan.ts
@@ -2,5 +2,9 @@ import instance from "@/shared/lib/axios";
 import { SloganFormValues } from "../model/schema";
 
 export const postSlogan = async (data: SloganFormValues) => {
-  return await instance.post("/slogan", data);
+  return await instance.post("/slogan", {
+    ...data,
+    grade: Number(data.grade),
+    classroom: Number(data.classroom),
+  });
 };
