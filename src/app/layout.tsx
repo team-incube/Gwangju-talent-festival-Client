25c55c93c063066edbdb017b8095846129e3ec8d
happytaeyoon <s23021@gsm.hs.kr>
2025-06-01 21:57:50 +0900

    add: meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <head>
+        <meta name="naver-site-verification" content="9b809c2b47126fcb57113f690708d63c39eb9681" />
         <Script
           strategy="beforeInteractive"
           src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_API_KEY}&libraries=services&autoload=false`}
