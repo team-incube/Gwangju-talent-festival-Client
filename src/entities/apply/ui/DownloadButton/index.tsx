9a50f066a2b57eeb735f013da7c69940e2dac54a
재욱 <haensol976520@gmail.com>
2025-06-12 09:06:54 +0900

    fix: 다운로드 로직 수정

--- src/entities/apply/ui/DownloadButton/index.tsx
+++ src/entities/apply/ui/DownloadButton/index.tsx
@@ -1,6 +1,6 @@
-
 import { DownloadIcon } from "@/shared/asset/svg/DownloadIcon";
 import { toast } from "sonner";
+import { colors } from "@/shared/utils/color";
 
 interface DownloadButtonProps {
   filePath: string;
@@ -8,14 +8,33 @@ interface DownloadButtonProps {
 }
 
 export const DownloadButton = ({ filePath, label }: DownloadButtonProps) => {
-  const handleDownload = () => {
-    const link = document.createElement('a');
-    link.href = filePath;
-    link.download = filePath.split('/').pop() || '';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-    toast.success("파일이 다운로드되었습니다.");
+  const handleDownload = async () => {
+    try {
+      const fileUrl = filePath.startsWith('/') ? filePath : `/${filePath}`;
+      
+      const response = await fetch(fileUrl);
+      
+      if (!response.ok) {
+        throw new Error(`${response.status}`);
+      }
+      
+      const blob = await response.blob();
+      const url = window.URL.createObjectURL(blob);
+      
+      const link = document.createElement('a');
+      link.href = url;
+      link.download = filePath.split('/').pop() || '';
+      document.body.appendChild(link);
+      link.click();
+      
+      window.URL.revokeObjectURL(url);
+      document.body.removeChild(link);
+      
+      toast.success("파일이 다운로드되었습니다.");
+    } catch (error) {
+      console.error( error);
+      toast.error("파일 다운로드에 실패했습니다.");
+    }
   };
 
   return (
@@ -24,7 +43,7 @@ export const DownloadButton = ({ filePath, label }: DownloadButtonProps) => {
       onClick={handleDownload}
     >
       {label}
-      <DownloadIcon color="#AC42CD" />
+      <DownloadIcon color={colors.main[600]} />
     </span>
   );
 };
