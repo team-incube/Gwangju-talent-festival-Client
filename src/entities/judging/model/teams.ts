df3fe867ae760771f303a7fb0cb81cea7249057e
koreahghg <koreahghg@gmail.com>
2026-07-14 14:11:37 +0900

    fix: getVisibleTeams에서 제외된 팀 ID 필터링 제거

--- src/entities/judging/model/teams.ts
+++ src/entities/judging/model/teams.ts
@@ -1,6 +1,4 @@
 import { Score } from "./score";
 
-const EXCLUDED_TEAM_ID = 11;
-
 export const getVisibleTeams = (scores: Score[]): Score[] =>
-  scores.filter(score => score.teamId !== EXCLUDED_TEAM_ID).sort((a, b) => a.teamId - b.teamId);
+  [...scores].sort((a, b) => a.teamId - b.teamId);
